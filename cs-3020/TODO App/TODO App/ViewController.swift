0a7fd7cb2bdf8b64b803ea57aeca485767a6f939
Zach <zach@Zachs-MacBook-Pro-2.local>
2015-09-16 15:59:52 -0600

    art quiz 2,3

---
+++ cs-3020/TODO App/TODO App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TODO App
+//
+//  Created by Zach Larsen on 9/15/15.
+//  Copyright (c) 2015 Zach Larsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
